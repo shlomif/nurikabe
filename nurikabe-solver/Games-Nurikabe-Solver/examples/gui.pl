1b2419786c62c7698859f530f25d094141fd4c0e
shlomif <shlomif@b384bcd7-cfd4-0310-aca0-d78b80f7b91b>
2008-10-04 20:47:15 +0000

    Added the input from the board.

--- nurikabe-solver/Games-Nurikabe-Solver/examples/gui.pl
+++ nurikabe-solver/Games-Nurikabe-Solver/examples/gui.pl
@@ -3,14 +3,48 @@
 use strict;
 use warnings;
 
+use blib;
+
 package NurikabeCanvas;
 
 use Wx ':everything';
 use Wx::Event qw(EVT_PAINT);
 use base 'Wx::Window';
 
+use Games::Nurikabe::Solver::Board;
+use Games::Nurikabe::Solver::Cell qw($NK_UNKNOWN $NK_WHITE $NK_BLACK);
+
 my $cell_width = 30;
 my $cell_height = 30;
+
+sub slurp
+{
+    my $filename = shift;
+    {
+        local $/;
+        open my $in, "<", $filename;
+        my $text = <$in>;
+        close($in);
+        return $text;
+    }
+}
+
+my $board = Games::Nurikabe::Solver::Board->load_from_string(
+    slurp(shift(@ARGV))
+);
+
+my %numbers = ();
+
+sub assign_board
+{
+    my $self = shift;
+
+    foreach my $island (@{$board->_islands()})
+    {
+        my $cell = $island->known_cells->[0];
+        $numbers{join(",", @$cell)} = $island->order();
+    }
+}
 
 sub new
 {
@@ -24,6 +58,8 @@ sub new
         Wx::Size->new($cell_width*9, $cell_height*9)
     );
 
+    $self->assign_board();
+
     EVT_PAINT( $self, \&OnPaint );
 
     return $self;
@@ -35,7 +71,7 @@ sub OnPaint
 
     my $dc = Wx::PaintDC->new($self);
 
-    my $black_pen = Wx::Pen->new(Wx::Colour->new(0,0,0), 3, wxSOLID());
+    my $black_pen = Wx::Pen->new(Wx::Colour->new(0,0,0), 4, wxSOLID());
 
     $dc->SetPen( $black_pen );
 
@@ -43,8 +79,41 @@ sub OnPaint
     {
         for my $x (0 .. 8)
         {
-            $dc->SetBrush(wxGREY_BRUSH());
-            $dc->DrawRectangle($cell_width*$x, $cell_height*$y, $cell_width, $cell_height);
+            my $status = $board->get_cell([$y,$x])->status();
+
+            if ($status eq $NK_UNKNOWN)
+            {
+                $dc->SetBrush(wxGREY_BRUSH());
+            }
+            elsif ($status eq $NK_WHITE)
+            {
+                $dc->SetBrush(wxWHITE_BRUSH());
+            }
+            elsif ($status eq $NK_BLACK)
+            {
+                $dc->SetBrush(wxBLACK_BRUSH());
+            }
+
+            my $p_x = $cell_width*$x;
+            my $p_y = $cell_height*$y;
+            $dc->DrawRectangle(
+                $p_x, $p_y, $cell_width, $cell_height
+            );
+
+            if (exists($numbers{join(",",$y,$x)}))
+            {
+                my $s = $numbers{join(",",$y,$x)};
+
+                my $c_x = $p_x + $cell_width/2;
+                my $c_y = $p_y + $cell_height/2;
+
+                my ($w, $h) = $dc->GetTextExtent($s);
+                $dc->DrawText(
+                    $s,
+                    $c_x - $w/2,
+                    $c_y - $h/2,
+                );
+            }
         }
     }
 }
